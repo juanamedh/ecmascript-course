b21e923312df7f10b14d0f68af286324cefd0af2
Juan Sanchez <58664932+juanamedh@users.noreply.github.com>
2022-09-17 15:48:29 -0700

    first commit

---
+++ src /es6/03- strings.js
@@ -0,0 +1,16 @@
+let hello = 'Hello';
+let world = 'Wold';
+let epicPhrase = hello + ' ' + world + '!';
+
+//Template Literals
+let epicPhraseV2 = `${hello} ${world}`;
+console.log(epicPhraseV2);
+
+// Multi-line Strings
+let lorem = 'This is a String \n' + 'This is another line';
+
+let lorem2 = `This is an epic phase
+this is the continuation of this epic phrase`;
+
+console.log(lorem);
+console.log(lorem2);
---
+++ src /es6/04-rest-spread.js
@@ -0,0 +1,29 @@
+// Arrays destructurin g
+
+let fruits = ['Apple', 'Banana']
+let [a,b] = fruits;
+console.log(a, fruits[1]);
+
+// Object desctructuring
+
+let user = {username: 'Oscar', age: 34};
+let {username, age} = user;
+console.log(username, user.age);
+
+// Spread operator
+
+let person = {name: 'oscar', age: 28};
+let country = 'MX';
+
+let data = {id: 1, ...person, country};
+console.log(data);
+
+// Rest
+
+function sum(num, ...values) {
+    console.log(values);
+    console.log(num + values[0]);
+    return num + values[0];
+};
+
+sum(1, 1, 2, 3);
