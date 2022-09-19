f424667a1c68647e81b2abb6a58d18130efffce7
Juan Sanchez <58664932+juanamedh@users.noreply.github.com>
2022-09-19 13:35:59 -0700

    finished generator exercise

---
+++ src /es6/09-generator.js
@@ -0,0 +1,16 @@
+
+function* iterate(arr) {
+    for( let value of arr) {
+        yield value
+    }
+}
+
+const it = iterate(['Oscar', 'Ana', 'Ulises', 'Jennifer']);
+console.log(it.next().value);
+console.log(it.next().value);
+console.log(it.next().value);
+console.log(it.next().value);
+console.log(it.next().value);
+console.log(it.next().value);
+console.log(it.next().value);
+console.log(it.next().value);
