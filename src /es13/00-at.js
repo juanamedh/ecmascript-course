cd8a68178eb95956d9ebcfa7ec4610aa5813ef0c
Juan Sanchez <58664932+juanamedh@users.noreply.github.com>
2022-09-20 14:03:33 -0700

    finished at method example excercise

--- src /es13/00-at.js
+++ src /es13/00-at.js
@@ -1,2 +1,6 @@
 // Base array
 const array = ["one", "two", "three", "four", "five", "six"];
+
+// at array method allows you to look for using element's index
+console.log(array[[array.length - 1]]);
+console.log(array.at(-1));
