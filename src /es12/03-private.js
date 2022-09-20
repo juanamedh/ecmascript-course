6b28c84c0576311bdaf7dc7b14a5b3e03821c112
Juan Sanchez <58664932+juanamedh@users.noreply.github.com>
2022-09-20 13:56:40 -0700

    Now you can make methods private with a #

--- src /es12/03-private.js
+++ src /es12/03-private.js
@@ -4,17 +4,23 @@ class user {
         this.age = age;
     }
     // Methods
-    speak() {
+    // add a # before the method's name to make it class private
+    #speak() {
         return 'Hello';
     }
     greeting() {
         return `${this.speak}, my name is ${this.name}`;
     }
 
-    get uAge() {
+    get #uAge() {
         return this.age;
     }
-    set uAge(n) {
+    set #uAge(n) {
         this.age = n;
     }
 }
+
+const babyloper1 = new user('Juan', 23);
+console.log(babyloper1.uAge);
+console.log(babyloper1.uAge = 20);
+console.log(babyloper1.greeting());
