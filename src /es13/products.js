dab018a72a80414af7829b037a53dcd8a27b14d8
Juan Sanchez <58664932+juanamedh@users.noreply.github.com>
2022-09-20 14:33:19 -0700

    finish product module w/ top level await

--- src /es13/products.js
+++ src /es13/products.js
@@ -1,3 +1,7 @@
 import fetch from "node-fetch"; 
 
-const 
+const response = await fetch('https://api.escuelajs.co/api/v1/products');
+const products = await response.json();
+
+export { products };
+
