6f3a20c0edb499f55da206b96a8e1382239ade79
Juan Sanchez <58664932+juanamedh@users.noreply.github.com>
2022-09-20 13:02:19 -0700

    fix typo

--- src /es11/04-globalthis.js
+++ src /es11/04-globalthis.js
@@ -1,6 +1,6 @@
 console.log(window); // browser
 console.log(global); // node
-console.log(selft); // webworker
+console.log(self); // webworker
 
 // We can now use globalThis insted of asking the context
 console.log(globalThis)
