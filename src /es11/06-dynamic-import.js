76346a7c73159f49e1127aefb348a6993f94a448
Juan Sanchez <58664932+juanamedh@users.noreply.github.com>
2022-09-20 13:23:46 -0700

    finished dynamic import excecise

--- src /es11/06-dynamic-import.js
+++ src /es11/06-dynamic-import.js
@@ -3,5 +3,5 @@ const button = document.getElementById('btn');
 button.addEventListener("click", async function() { 
     const module = await import('./module.js');
     console.log(module);
-    module.hello;
+    module.hello();
 });
